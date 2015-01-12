2056c886eb12b36793da650bd2846cc0f333ba7d
MT <e1025496@student.tuwien.ac.at>
2015-01-12 22:58:47 +0100

    articles move again when released, Player and PlayerList classes created

---
+++ Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Surface.Presentation.Controls;
+
+/*
+ * represents a player of the game
+ */
+
+namespace PhotoPaint
+{
+    class Player
+    {
+
+        public int points;
+
+        public int orientation; // specifies orientation of elements in degrees to show something to this user
+
+        public ScatterViewItem currentImage; // image currently in the image slot
+        public ScatterViewItem currentText; // text currently in the text slot
+
+        public List<Article> finishedArticles; // articles to show in the list of finished articles
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        public Player(int orientation)
+        {
+            this.orientation = orientation;
+            points = 0;
+            finishedArticles = new List<Article>();
+        }
+
+        /// <summary>
+        /// reset player data
+        /// </summary>
+        public void resetPlayer()
+        {
+            points = 0;
+            finishedArticles = new List<Article>();
+        }
+
+    }
+}
---
+++ PlayerList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PhotoPaint
+{
+    class PlayerList
+    {
+
+        public List<Player> players;
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="numberOfPlayers">number of players to be able to play the game</param>
+        public PlayerList(int numberOfPlayers) 
+        {
+            players = new List<Player>();
+            for (int i = 0; i < numberOfPlayers; i++)
+            {
+                players.Add(new Player(i * 90));
+            }
+        }
+
+        /// <summary>
+        /// reset all players
+        /// </summary>
+        public void resetAll()
+        {
+            foreach (Player player in players)
+            {
+                player.resetPlayer();
+            }
+        }
+
+    }
+}
